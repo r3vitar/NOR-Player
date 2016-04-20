bc968bfdb7d3f59a5b8de14dd202c424040ca6eb
PhilippRadler <philipp.radler1878@gmail.com>
2016-04-20 10:35:48 +0200

    Added files via upload

---
+++ NOR-Player/src/nor/player/StyledCell.java
@@ -0,0 +1,37 @@
+package nor.player;
+
+import javafx.util.*;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.*;
+import javafx.event.*;
+
+/**
+ * A cell with CSS class chosen by value.
+ *
+ *
+ * @author Philipp Radler
+ * special thanks to Bernhard Bodenstorfer for helping me design this class
+ */
+public class StyledCell<T> extends TableCell<LineItem, T> {
+    private int playingIndex = 0;
+    @Override
+    public void updateItem(final T item, final boolean empty) {
+        super.updateItem(item, empty);
+        if (empty || null == item) {
+            setText(null);
+            setGraphic(null);
+        } else {
+            TableRow<LineItem> tr = getTableRow();
+            if (item.equals(playingIndex+1)) {
+                tr.getStyleClass().add("emphasisedRow");
+            } else {
+                tr.getStyleClass().remove("emphasisedRow");
+            }
+            setText(item.toString());
+        }
+    }
+    
+    public void setIndex(int playingIndex){
+        this.playingIndex = playingIndex;
+    }
+}
---
+++ NOR-Player/src/nor/player/StyledCellFactory.java
@@ -0,0 +1,27 @@
+package nor.player;
+
+import javafx.util.*;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.*;
+import javafx.event.*;
+
+/**
+ * Equips cells with CSS class
+ *
+ * @author Philipp Radler
+ * special thanks to Bernhard Bodenstorfer for helping me design this class
+ */
+public class StyledCellFactory<T> implements Callback<TableColumn<LineItem, T>, TableCell<LineItem, T>> {
+    private int playingIndex = 0;
+    
+    @Override
+    public TableCell<LineItem, T> call(final TableColumn<LineItem, T> column) {
+        StyledCell<T> sc = new StyledCell<T>();
+        sc.setIndex(playingIndex);
+        return sc;
+    }
+    
+    public void setIndex(int playingIndex){
+        this.playingIndex = playingIndex;
+    }
+}
